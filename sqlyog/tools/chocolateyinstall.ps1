debe3d41c69e8a7af0d0d6c95920c11c9cb2adde
Brian Mego <brianmego@gmail.com>
2013-01-23 20:54:23 -0600

    4 packages so far

---
+++ sqlyog/tools/chocolateyinstall.ps1
@@ -0,0 +1,4 @@
+﻿#try { #error handling is only necessary if you need to do anything in addition to/instead of the main helpers
+  # main helpers - these have error handling tucked into them so they become the only line of your script if that is all you need.
+  # installer, will assert administrative rights
+  Install-ChocolateyPackage 'sqlyog' 'exe' '/S' 'http://sqlyog.googlecode.com/files/SQLyog-10.3.0-1Community.exe'
---
+++ steam/tools/chocolateyInstall.ps1
@@ -0,0 +1,4 @@
+﻿#try { #error handling is only necessary if you need to do anything in addition to/instead of the main helpers
+  # main helpers - these have error handling tucked into them so they become the only line of your script if that is all you need.
+  # installer, will assert administrative rights
+  Install-ChocolateyPackage 'steam' 'msi' '/quiet' 'http://cdn.steampowered.com/download/SteamInstall.msi'
