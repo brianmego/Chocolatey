42b74bab4bdbf687578490e6b8422a58430cb82a
Brian Mego <brianmego@gmail.com>
2013-02-22 07:33:47 -0600

    Upgraded SQLYog to 11.0.1

--- sqlyog/x64/tools/chocolateyinstall.ps1
+++ sqlyog/x64/tools/chocolateyinstall.ps1
@@ -1,2 +1,2 @@
 ﻿# installer, will assert administrative rights
-Install-ChocolateyPackage 'sqlyog.x64' 'exe' '/S' 'https://sqlyog.googlecode.com/files/SQLyog-11.0.0-2.x64Community.exe'
+Install-ChocolateyPackage 'sqlyog.x64' 'exe' '/S' 'https://sqlyog.googlecode.com/files/SQLyog-11.0.1-0.x64Community.exe'
