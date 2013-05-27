cbc91467f13dee890c239163a4ae765fda32cf04
TomOne <user.email@fake.com>
2013-05-27 02:46:12 +0200

    added support for other languages to steam package, changed charset of nuspec file to UTF-8

--- steam/tools/chocolateyInstall.ps1
+++ steam/tools/chocolateyInstall.ps1
@@ -1,4 +1,21 @@
-﻿#try { #error handling is only necessary if you need to do anything in addition to/instead of the main helpers
-  # main helpers - these have error handling tucked into them so they become the only line of your script if that is all you need.
-  # installer, will assert administrative rights
-  Install-ChocolateyPackage 'steam' 'msi' '/quiet' 'http://cdn.steampowered.com/download/SteamInstall.msi'
+﻿try {
+
+    $langcode = (Get-Culture).Name # Get language and country code
+    $langcode = "$langcode" -replace '-[a-z]{2}', '' # Remove country code
+    #$langcode = "en" # langcode override for testing
+
+    $languageAppendix = @{
+        "fr" = "_French";
+        "de" = "_German";
+        "es" = "_Spanish";
+    } # These 3 languages have a special installer. All the other languages have the standard installer named "SteamInstall.msi"
+
+    $language = $languageAppendix[$langcode]
+    $url = "http://cdn.steampowered.com/download/SteamInstall$language.msi"
+
+    Install-ChocolateyPackage 'steam' 'msi' '/quiet' $url
+    Write-ChocolateySuccess $name
+} catch {
+  Write-ChocolateyFailure $name $($_.Exception.Message)
+  throw
+}
