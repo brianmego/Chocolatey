a4c5950f3ec30b8fa839db98a32477e5b38dee28
Brian Mego <brianmego@gmail.com>
2013-07-24 09:10:34 -0500

    Bad naming on last commit

--- sqlyog/x64/tools/chocolateyinstall.ps1
+++ sqlyog/x64/tools/chocolateyinstall.ps1
@@ -1,2 +1,2 @@
 ﻿# installer, will assert administrative rights
-Install-ChocolateyPackage 'sqlyog.x64' 'exe' '/S' 'https://sqlyog.googlecode.com/files/SQLyog-11.1.0-4.x64Community.exe'
+Install-ChocolateyPackage 'sqlyog.x64' 'exe' '/S' 'https://sqlyog.googlecode.com/files/SQLyog-11.2.0-4.x64Community.exe'
