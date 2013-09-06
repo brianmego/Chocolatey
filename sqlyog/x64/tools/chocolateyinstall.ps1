8ac7b656a7f2f469958d2d8e683dfa5ab74e328e
Brian Mego <brianmego@gmail.com>
2013-09-06 10:03:43 -0500

    Updated sqlyog to 11.2.4.0

--- sqlyog/x64/tools/chocolateyinstall.ps1
+++ sqlyog/x64/tools/chocolateyinstall.ps1
@@ -1,2 +1,2 @@
 ﻿# installer, will assert administrative rights
-Install-ChocolateyPackage 'sqlyog.x64' 'exe' '/S' 'https://sqlyog.googlecode.com/files/SQLyog-11.2.2-1.x64Community.exe'
+Install-ChocolateyPackage 'sqlyog.x64' 'exe' '/S' 'https://sqlyog.googlecode.com/files/SQLyog-11.2.4-0.x64Community.exe'
--- sqlyog/x86/tools/chocolateyinstall.ps1
+++ sqlyog/x86/tools/chocolateyinstall.ps1
@@ -1,2 +1,2 @@
 ﻿# installer, will assert administrative rights
-Install-ChocolateyPackage 'sqlyog' 'exe' '/S' 'https://sqlyog.googlecode.com/files/SQLyog-11.2.2-1.x86Community.exe'
+Install-ChocolateyPackage 'sqlyog' 'exe' '/S' 'https://sqlyog.googlecode.com/files/SQLyog-11.2.4-0.x86Community.exe'
