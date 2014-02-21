aa76a890ab3c4122c9f3af1dd5562be398f07d59
brianmego <brianmego@gmail.com>
2014-02-21 08:54:05 -0600

    Swish 0.8.0

--- swish/tools/chocolateyInstall.ps1
+++ swish/tools/chocolateyInstall.ps1
@@ -1,2 +1,2 @@
-﻿Install-ChocolateyPackage 'swish' 'exe' '/q' 'http://downloads.sourceforge.net/project/swish/swish/swish-0.7.3/swish-0.7.3.exe' 
+﻿Install-ChocolateyPackage 'swish' 'exe' '/q' 'http://downloads.sourceforge.net/project/swish/swish/swish-0.8.0/swish-0.8.0.exe' 
   
