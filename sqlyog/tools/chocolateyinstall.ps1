47f9382640115f056a2afc69684497b15315b742
Brian Mego <brianmego@gmail.com>
2014-04-14 09:16:49 -0500

    Sqlyog 11.41

--- sqlyog/tools/chocolateyinstall.ps1
+++ sqlyog/tools/chocolateyinstall.ps1
@@ -1,2 +1,2 @@
 ﻿# installer, will assert administrative rights
-Install-ChocolateyPackage 'sqlyog' 'exe' '/S' 'https://s3.amazonaws.com/SQLyog_Community/SQLyog+11.4/SQLyog-11.4.0-0.x86Community.exe' 'https://s3.amazonaws.com/SQLyog_Community/SQLyog+11.4/SQLyog-11.4.0-0.x64Community.exe'
+Install-ChocolateyPackage 'sqlyog' 'exe' '/S' 'https://s3.amazonaws.com/SQLyog_Community/SQLyog+11.41/SQLyog-11.4.1-0.x86Community.exe' 'https://s3.amazonaws.com/SQLyog_Community/SQLyog+11.41/SQLyog-11.4.1-0.x64Community.exe'
