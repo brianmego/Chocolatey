fe057fb4c7118ea6221fccc118b2c1b292aaee57
Brian Mego <brianmego@gmail.com>
2014-06-21 15:43:47 -0500

    Dexpot 1613

--- dexpot/tools/chocolateyInstall.ps1
+++ dexpot/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿#try { #error handling is only necessary if you need to do anything in addition to/instead of the main helpers
   # main helpers - these have error handling tucked into them so they become the only line of your script if that is all you need.
   # installer, will assert administrative rights
-  Install-ChocolateyPackage 'dexpot' 'exe' '/S' 'http://www.dexpot.de/download/dexpot_1612_r2416.exe' 
+  Install-ChocolateyPackage 'dexpot' 'exe' '/S' 'http://www.dexpot.de/download/dexpot_1613_r2429.exe' 
   
