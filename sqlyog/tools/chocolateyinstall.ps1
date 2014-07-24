934d552a04ce09d866e85e8fd9b63978ef570f27
Brian Mego <brianmego@gmail.com>
2014-07-24 18:10:38 -0500

    Updated steam and sqlyog packages

--- sqlyog/tools/chocolateyinstall.ps1
+++ sqlyog/tools/chocolateyinstall.ps1
@@ -1,2 +1,2 @@
 ﻿# installer, will assert administrative rights
-Install-ChocolateyPackage 'sqlyog' 'exe' '/S' 'https://s3.amazonaws.com/SQLyog_Community/SQLyog+11.51/SQLyog-11.5.1-0.x86Community.exe' 'https://s3.amazonaws.com/SQLyog_Community/SQLyog+11.51/SQLyog-11.5.1-0.x64Community.exe'
+Install-ChocolateyPackage 'sqlyog' 'exe' '/S' 'https://s3.amazonaws.com/SQLyog_Community/SQLyog+11.52/SQLyog-11.5.2-0.x86Community.exe' 'https://s3.amazonaws.com/SQLyog_Community/SQLyog+11.52/SQLyog-11.5.2-0.x64Community.exe'
