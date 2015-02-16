e947b68acd4371a2f86feae0c8aa1800e3bfae7e
Brian Mego <bmego@predictivescience.com>
2015-02-16 15:35:32 -0600

    Updates sqlyog package to bring it in line with new chocolatey requirements

--- sqlyog/tools/chocolateyinstall.ps1
+++ sqlyog/tools/chocolateyinstall.ps1
@@ -1,2 +1,14 @@
-﻿# installer, will assert administrative rights
-Install-ChocolateyPackage 'sqlyog' 'exe' '/S' 'https://s3.amazonaws.com/SQLyog_Community/SQLyog+11.52/SQLyog-11.5.2-0.x86Community.exe' 'https://s3.amazonaws.com/SQLyog_Community/SQLyog+11.52/SQLyog-11.5.2-0.x64Community.exe'
+﻿$packageName = 'sqlyog'
+$installerType = 'EXE'
+$url = 'https://s3.amazonaws.com/SQLyog_Community/SQLyog+12.08/SQLyog-12.0.8-0.x86Community.exe'
+$url64 = 'https://s3.amazonaws.com/SQLyog_Community/SQLyog+12.08/SQLyog-12.0.8-0.x64Community.exe'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+try {
+	Install-ChocolateyPackage $packageName $installerType $silentArgs $url $url64 -validExitCodes $validExitCodes
+	Write-ChocolateySuccess $packageName
+} catch {
+	Write-ChocolateyFailure $packageName $($_.Exception.Message)
+	throw	
+}
