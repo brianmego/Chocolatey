ea850883bd921d7b196314a6184b902c8ed7a4df
Brian Mego <bmego@predictivescience.com>
2015-02-16 15:35:15 -0600

    Updates steam package to bring it in line with new chocolatey requirements

--- steam/tools/chocolateyInstall.ps1
+++ steam/tools/chocolateyInstall.ps1
@@ -1,1 +1,13 @@
-﻿Install-ChocolateyPackage 'steam' 'exe' '/S' "http://media.steampowered.com/client/installer/SteamSetup.exe"
+﻿$packageName = 'steam'
+$installerType = 'EXE'
+$url = 'http://media.steampowered.com/client/installer/SteamSetup.exe'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+try {
+	Install-ChocolateyPackage $packageName $installerType $silentArgs $url -validExitCodes $validExitCodes
+	Write-ChocolateySuccess $packageName
+} catch {
+	Write-ChocolateyFailure $packageName $($_.Exception.Message)
+	throw	
+}
