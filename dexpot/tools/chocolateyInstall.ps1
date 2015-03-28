252e9496d5430e3aedeb4197cfabef35183ff460
Brian Mego <brianmego@gmail.com>
2015-03-28 15:22:51 -0500

    Dexpot version 1.6.14

--- dexpot/tools/chocolateyInstall.ps1
+++ dexpot/tools/chocolateyInstall.ps1
@@ -1,5 +1,13 @@
-﻿#try { #error handling is only necessary if you need to do anything in addition to/instead of the main helpers
-  # main helpers - these have error handling tucked into them so they become the only line of your script if that is all you need.
-  # installer, will assert administrative rights
-  Install-ChocolateyPackage 'dexpot' 'exe' '/S' 'http://www.dexpot.de/download/dexpot_1613_r2429.exe' 
-  
+﻿$packageName = 'dexpot'
+$installerType = 'EXE'
+$url = 'http://dexpot.de/download/dexpot_1614_r2439.exe'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+try {
+	Install-ChocolateyPackage $packageName $installerType $silentArgs $url -validExitCodes $validExitCodes
+	Write-ChocolateySuccess $packageName
+} catch {
+	Write-ChocolateyFailure $packageName $($_.Exception.Message)
+	throw	
+}
