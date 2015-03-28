746a79736642cf421e626be76e2f6747e15ac527
Brian Mego <brianmego@gmail.com>
2015-03-28 16:18:32 -0500

    Adds sqlyog 12.09

--- sqlyog/tools/chocolateyinstall.ps1
+++ sqlyog/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'sqlyog'
 $installerType = 'EXE'
-$url = 'https://s3.amazonaws.com/SQLyog_Community/SQLyog+12.08/SQLyog-12.0.8-0.x86Community.exe'
-$url64 = 'https://s3.amazonaws.com/SQLyog_Community/SQLyog+12.08/SQLyog-12.0.8-0.x64Community.exe'
+$url = 'https://s3.amazonaws.com/SQLyog_Community/SQLyog+12.09/SQLyog-12.0.9-0.x86Community.exe'
+$url64 = 'https://s3.amazonaws.com/SQLyog_Community/SQLyog+12.09/SQLyog-12.0.9-0.x64Community.exe'
 $silentArgs = '/S'
 $validExitCodes = @(0)
 
