8c8961ebbc7043f5e4942b825620b1bd658848d1
Brian Mego <brian.mego@q2ebanking.com>
2016-09-04 14:38:19 -0500

    Edits copyright on steam

--- README.md
+++ README.md
@@ -6,5 +6,5 @@ Chocolatey
 <ul>
 <li>Instructions on adding packages are at https://github.com/chocolatey/choco/wiki/CreatePackages</li>
 <li>Instead of using warmup as per the above link, it is simpler to just copy one of the existing folders in this repo and rename it</li>
-<li>To use commandline cpush, you must cinst nuget.commandline and add your API key from Chocolatey with the command "nuget setApiKey [your-key-here] -Source http://chocolatey.org/"</li>
+<li>To use commandline cpush, you must cinst nuget.commandline and add your API key from Chocolatey with the command `nuget setApiKey [your-key-here] -Source http://chocolatey.org/`</li>
 </ul>
--- packages/steam/tools/chocolateyUninstall.ps1
+++ packages/steam/tools/chocolateyUninstall.ps1
@@ -5,15 +5,9 @@ $processor = Get-WmiObject Win32_Processor
 $is64bit = $processor.AddressWidth -eq 64
 $validExitCodes = @(0)
 
-try {
-	if ($is64bit) {
-		$unpath = "${Env:ProgramFiles(x86)}\Steam\uninstall.exe"
-	} else {
-		$unpath = "$Env:ProgramFiles\Steam\uninstall.exe"
-	}
-	Uninstall-ChocolateyPackage $packageName $installerType $silentArgs $unpath -validExitCodes $validExitCodes
-	Write-ChocolateySuccess $packageName
-} catch {
-	Write-ChocolateyFailure $packageName $($_.Exception.Message)
-	throw	
+if ($is64bit) {
+    $unpath = "${Env:ProgramFiles(x86)}\Steam\uninstall.exe"
+} else {
+    $unpath = "$Env:ProgramFiles\Steam\uninstall.exe"
 }
+Uninstall-ChocolateyPackage $packageName $installerType $silentArgs $unpath -validExitCodes $validExitCodes
