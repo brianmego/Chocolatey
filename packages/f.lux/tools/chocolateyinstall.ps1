267e3deda6bcf468ee783e014255f92142522c6a
Link <Link-Satonaka@users.noreply.github.com>
2016-11-30 19:17:31 -0500

    Install to chocolatey toolsdir
    f.lux's native installer is broken and unconfigurable. Luckily, we don't need it. By simply using the chocolatey install zip function, we can cleanly install f.lux system wide.

--- packages/f.lux/tools/chocolateyinstall.ps1
+++ packages/f.lux/tools/chocolateyinstall.ps1
@@ -1,18 +1,9 @@
-$ErrorActionPreference = 'Stop'
- 
-$packageName = 'f.lux'
-$url32       = 'http://stereopsis.com/flux/flux-setup.exe'
-$checksum32  = '99F6A5FCF8C6789FF4D69A98B6CB1AF9296A76F210C01A6C8A0716EF79134F2F'
- 
 $packageArgs = @{
-  packageName            = $packageName
-  fileType               = 'exe'
-  url                    = $url32
-  silentArgs             = '/S'
-  checksum               = $checksum32
+  packageName            = 'f.lux'
+  url                    = 'http://stereopsis.com/flux/flux-setup.exe'
+  checksum               = '99F6A5FCF8C6789FF4D69A98B6CB1AF9296A76F210C01A6C8A0716EF79134F2F'
   checksumType           = 'sha256'
-  validExitCodes         = @(0)
-  registryUninstallerKey = $packageName
+  UnzipLocation 	 = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 }
  
-Install-ChocolateyPackage @packageArgs
+Install-ChocolateyZipPackage @packageArgs
