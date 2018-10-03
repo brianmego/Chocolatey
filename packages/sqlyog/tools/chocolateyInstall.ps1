33068a6f5d6ac4daec33edab2670d709afe819d5
Werner Mayer <werner@thinkit.co.nz>
2018-10-03 17:41:01 +1300

    Version URL and package update

--- packages/sqlyog/tools/chocolateyInstall.ps1
+++ packages/sqlyog/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'sqlyog'
 $installerType = 'EXE'
-$url = 'https://s3.amazonaws.com/SQLyog_Community/SQLyog+12.09/SQLyog-12.0.9-0.x86Community.exe'
-$url64 = 'https://s3.amazonaws.com/SQLyog_Community/SQLyog+12.09/SQLyog-12.0.9-0.x64Community.exe'
+$url = 'https://s3.amazonaws.com/SQLyog_Community/SQLyog+13.1.1/SQLyog-13.1.1-0.x86Community.exe'
+$url64 = 'https://s3.amazonaws.com/SQLyog_Community/SQLyog+13.1.1/SQLyog-13.1.1-0.x64Community.exe'
 $silentArgs = '/S'
 
 Install-ChocolateyPackage $packageName $installerType $silentArgs $url $url64
